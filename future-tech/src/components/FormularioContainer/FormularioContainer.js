67e7af1948e3540afb8ab81e44cd799b1acba15b
Brian Mello <brianmmello@gmail.com>
2019-12-19 13:47:36 -0300

    Atualizei o formulario e um pouco da responsividade

--- future-tech/src/components/FormularioContainer/FormularioContainer.js
+++ future-tech/src/components/FormularioContainer/FormularioContainer.js
@@ -4,59 +4,72 @@ import { withStyles } from '@material-ui/core/styles';
 
 
 const Titulo = styled.h1`
-text-align:center;
+  text-align:center;
 `
 
 const ContainerForm = styled.div `
-width: 500px;
-height: 480px;
-border-radius:10px;
-background-color:#C76103;
-margin:auto;
-display:flex;
-flex-direction:column;
-margin-bottom:30px;
+  width: 95vw;
+  height: 60vh;
+  max-width: 500px;
+  max-height: 540px;
+  border-radius:10px;
+  background-color:#C76103;
+  margin:auto;
+  display:flex;
+  flex-direction:column;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-bottom:30px; 
 
 `
-const Label = styled.label`
-color:black;
-padding-left:80px;
-padding-top:30px;
-font-weight:bold;
-margin-right:10px;
 
-`
 const ContainerLabels = styled.div`
-margin-top:20px;
-margin-bottom:5px;
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
 `
 
 const Input = styled.input`
-border:none;
-border-radius:5px;
-width:200px;
-height: 18px;
+  border:none;
+  border-radius:5px;
+  width:50vw;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 200px;
 `
 
-const InputDescricao = styled.input`
-border:none;
-border-radius:5px;
-width:200px;
-height: 60px;
+const InputDescricao = styled.textarea`
+  border:none;
+  border-radius:5px;
+  width:200px;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 200px;
 `
 
 const Botao = styled.button`
-border:none;
-background-color:white;
-color:#C76103;
-cursor:pointer;
-width:auto;
-border: solid 2px black;
-margin:auto;
-border-radius: 10px;
-font-weight:bold;
-padding:10px;
-font-weight:bold;
+  background-color:white;
+  color:#C76103;
+  width:auto;
+  height: 30px;
+  margin:auto;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  font-weight: bolder;
+  outline: 0;
+  :hover {
+    cursor: pointer;
+    color: white;
+    background-color: gray;
+  }
 `
 
 class FormularioContainer extends React.Component{
@@ -106,44 +119,34 @@ class FormularioContainer extends React.Component{
           <Titulo>Cadastro de Produtos</Titulo>
           <ContainerForm>
             <ContainerLabels>
-              <Label>Título Produto:</Label>
-
-              <Input type="text" value={this.state.titulo} onChange={this.onChangeTitulo}></Input>
-
+              <h4>Título</h4>
+              <Input type="text" value={this.state.titulo} onChange={this.onChangeTitulo} placeholder = " Digite o Nome do produto"/>
             </ContainerLabels>
 
             <ContainerLabels>
-              <Label>Imagem Produto:</Label>
-
-              <Input type="text" value={this.state.imagem} onChange={this.onChangeImagem}></Input>
-
-            </ContainerLabels>
-
-            <ContainerLabels>
-              <Label>Descrição Produto:</Label>
-              <InputDescricao type="textarea" value={this.state.descricao} onChange={this.onChangeDescricao}></InputDescricao>
+              <h4>Descrição Produto:</h4>
+              <InputDescricao type="textarea" value={this.state.descricao} onChange={this.onChangeDescricao} placeholder = " Descreva o produto"/>
             </ContainerLabels>
            
             <ContainerLabels>
-              <Label>Valor Produto:</Label>
-
-              <Input type="number" value={this.state.valor} onChange={this.onChangeValor}></Input>
-
+              <h4>Valor Produto:</h4>
+              <Input type="number" value={this.state.valor} onChange={this.onChangeValor} placeholder = " Digite o produto"/>
             </ContainerLabels>
 
             <ContainerLabels>
-              <Label>Método de Pagamento:</Label>
-              <Input type="text" value={this.state.pagamento} onChange={this.onChangePagamento}></Input>
+              <h4>Método de Pagamento:</h4>
+              <select>
+                <option>Método de Pagamento</option>
+                <option>Cartão</option>
+                <option>Boleto</option>
+              </select>
             </ContainerLabels>
-            
             
             <ContainerLabels>
-              <Label>Prazo de Entrega:</Label>
-              <Input type="text" value={this.state.entrega} onChange={this.onChangeEntrega}></Input>
+              <h4>Prazo de Entrega:</h4>
+              <Input type="text" value={this.state.entrega} onChange={this.onChangeEntrega} placeholder = " Digite o prazo de entrega"/>
             </ContainerLabels>
-            
-            <Botao>Cadastrar Produto</Botao>
-       
+            <Botao>Cadastrar Produto</Botao>  
           </ContainerForm>
       </div>
     )
